c2892ac34f1be05a08634fe7bfb2df3510e1dd37
svenfulen <sfulen99@gmail.com>
2021-01-10 15:43:49 -0600

    Finished scraping data from Transactions for 235/236

--- TABC_Forms.py
+++ TABC_Forms.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 from datetime import date, timedelta, datetime
 from pandas.io import excel
 from openpyxl import load_workbook
@@ -10,13 +9,6 @@ invPath = 'Inventory.xlsx'
 productsPath = 'Products.xlsx'
 retailerCustomersPath = 'RetailerCustomers.xlsx'
 transactionsPath = 'Transactions.xlsx'
-
-
-# is a transaction date within the previous month? boolean
-def is_current_reporting_period(transaction_date):
-    last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
-    start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
-    return start_day_of_prev_month <= transaction_date <= last_day_of_prev_month
 
 
 def get_previous_report_name_prefix():
@@ -32,6 +24,13 @@ reports_rootdir = 'C:/Users/svenf/Desktop/beer/reportsdir/'
 
 previous_tabc_235_filename = reports_rootdir + get_previous_report_name_prefix() + 'c-235.xlsx'
 previous_tabc_236_filename = reports_rootdir + get_previous_report_name_prefix() + 'c-236.xlsx'
+
+
+# is a transaction date within the previous month? boolean
+def is_current_reporting_period(transaction_date):
+    last_day_of_prev_month = date.today().replace(day=1) - timedelta(days=1)
+    start_day_of_prev_month = date.today().replace(day=1) - timedelta(days=last_day_of_prev_month.day)
+    return start_day_of_prev_month <= transaction_date <= last_day_of_prev_month
 
 
 def new235():  # make a blank copy of a 235 form for the current month and year
@@ -71,16 +70,15 @@ class Transaction:
 
         self.is_off_premise = str(is_off_premise)
         self.is_retailer_sale = is_retailer_sale  # 0 or 1
-        self.product_list_index = 0  # Index of the product in the product list array. TBD
 
 
 class Product:
-    def __init__(self, brandName, productID, isLiquor):  # Product name, product ID, is it liquor or not (0 or 1)?
+    def __init__(self, brand_name, product_id, is_liquor):  # Product name, product ID, is it liquor or not (0 or 1)?
         self.alphabetical_order = 0  # Used to find which row of the tax form that the product data will be written to.
-        self.brandName = str(brandName)
-        self.productID = str(productID)
-        self.isLiquor = isLiquor  # TODO: change this to a boolean type
-        self.ONP_total_gallons_sold = 0  # ON PREMISE SALES total gallons (will need to convert units to gallons before inputting)
+        self.brandName = str(brand_name)
+        self.productID = str(product_id)
+        self.isLiquor = is_liquor  # TODO: change this to a boolean type
+        self.ONP_total_gallons_sold = 0  # ON PREMISE SALES total gallons
         self.OFFP_total_gallons_sold = 0  # OFF PREMISE SALES
         self.RTL_sixth_barrel_sold = 0  # RETAILER SALES (add different container types below this line)
         self.RTL_twentytwo_oz_bottle_sold = 0
@@ -89,34 +87,34 @@ class Product:
 
 def generate_transaction_list():  # Generate a list of Transaction objects with data needed for both forms.
     transactions = []
-    dfTransactions = pd.io.excel.read_excel(transactionsPath)
+    df_transactions = pd.io.excel.read_excel(transactionsPath)
     # dfProducts = pd.io.excel.read_excel(productsPath)
 
-    for row in dfTransactions.itertuples(index=False):  # iterate through all rows in dataframe
-        if is_current_reporting_period(row[dfTransactions.columns.get_loc('Date')]):  # Getting data into local vars
-            productID = row[dfTransactions.columns.get_loc('ProductID')]  # Getting product ID into local var
-            outputIndividualContainerSize = row[
-                dfTransactions.columns.get_loc('ContainerSize')]  # Getting container size #
-            outputContainerUnits = row[
-                dfTransactions.columns.get_loc('ContainerUnits')]  # Getting units (oz or gallons?)
-            outputNumberUnits = row[dfTransactions.columns.get_loc('NumUnits')]  # Getting number of containers sold
-            isOffPremise = row[dfTransactions.columns.get_loc('OffPrem')]  # is it off premise or on premise
-            if isOffPremise == "TRUE":
-                isOffPremise = 1
-            elif isOffPremise == "FALSE":
-                isOffPremise = 0
-            internalCustomerID = row[dfTransactions.columns.get_loc('InternalCustomerID')]  # internal customer id
-            cxid = int(internalCustomerID)  # Converting internal customer ID to an int.
+    for row in df_transactions.itertuples(index=False):  # iterate through all rows in dataframe
+        if is_current_reporting_period(row[df_transactions.columns.get_loc('Date')]):  # Getting data into local vars
+            output_product_id = row[df_transactions.columns.get_loc('ProductID')]  # Getting product ID into local var
+            output_individual_container_size = row[
+                df_transactions.columns.get_loc('ContainerSize')]  # Getting container size #
+            output_container_units = row[
+                df_transactions.columns.get_loc('ContainerUnits')]  # Getting units (oz or gallons?)
+            output_number_units = row[df_transactions.columns.get_loc('NumUnits')]  # Getting number of containers sold
+            is_off_premise = row[df_transactions.columns.get_loc('OffPrem')]  # is it off premise or on premise
+            if is_off_premise == "TRUE":
+                is_off_premise = 1
+            elif is_off_premise == "FALSE":
+                is_off_premise = 0
+            internal_customer_id = row[df_transactions.columns.get_loc('InternalCustomerID')]  # internal customer id
+            cxid = int(internal_customer_id)  # Converting internal customer ID to an int.
             is_rtl_sale = 0  # boolean, 0 or 1
             if cxid != 1:  # If internal customer ID is not 1, it's a retailer sale.
                 is_rtl_sale = 1
             elif cxid == 1:
                 is_rtl_sale = 0
-            transactions.append(Transaction(product_id=productID,
-                                            individual_container_size=outputIndividualContainerSize,
-                                            container_units=outputContainerUnits,
-                                            number_units=outputNumberUnits,
-                                            is_off_premise=isOffPremise,
+            transactions.append(Transaction(product_id=output_product_id,
+                                            individual_container_size=output_individual_container_size,
+                                            container_units=output_container_units,
+                                            number_units=output_number_units,
+                                            is_off_premise=is_off_premise,
                                             is_retailer_sale=is_rtl_sale)
                                 )
     return transactions
@@ -129,7 +127,7 @@ def test_transaction_list(transaction_list_name):
         print(str(test_list[x].number_units) + " of " + str(test_list[x].individual_container_size) + test_list[x].container_units)
         print("Off premise: " + test_list[x].is_off_premise)
         if test_list[x].is_retailer_sale == 1:
-            print ("(Retailer sale)")
+            print("(Retailer sale)")
         print("\n")
 
 
@@ -158,8 +156,8 @@ def generate_product_list(product_book):  # Put file path / filename for Product
     return product_list
 
 
-def test_product_list(pbook):
-    test_list = generate_product_list(pbook)
+def test_product_list(given_array):
+    test_list = given_array
     for x in range(0, len(test_list)):
         print("BRAND NAME:")
         print(test_list[x].brandName)
@@ -167,6 +165,11 @@ def test_product_list(pbook):
         print(test_list[x].productID)
         print(" IS LIQUOR:")
         print(test_list[x].isLiquor)
+        print("Total Gallons Sold to Retailers:" + str(test_list[x].RTL_total_gallons_sold))
+        print("Sixth Barrels sold to Retailers:" + str(test_list[x].RTL_sixth_barrel_sold))
+        print("22oz Bottles sold to Retailers:" + str(test_list[x].RTL_twentytwo_oz_bottle_sold))
+        print("Total gallons sold On premise consumption:" + str(test_list[x].ONP_total_gallons_sold))
+        print("Total gallons sold Off premise consumption:" + str(test_list[x].OFFP_total_gallons_sold))
         print("\n")
 
 
@@ -235,120 +238,51 @@ class Current236:  # Class that holds data to be put in the new 235 form.
         self.tax_due_state = 0
 
 
-array_transaction_data = generate_transaction_list()
-array_products = generate_product_list("Products.xlsx")
+array_transaction_data = generate_transaction_list()  # List of transactions in the current reporting period.
+array_products = generate_product_list("Products.xlsx")  # List of products
+product_id_dict = {p.productID: p for p in array_products}  # Creates a dictionary of product IDs that point to objects.
 prev_235_form = PreviousForm(previous_tabc_235_filename)  # Creating object using filename as parameter.
+prev_236_form = PreviousForm(previous_tabc_236_filename)
+current_235_form = Current235()
+current_236_form = Current236()
 
-test_transaction_list(array_transaction_data)
-'''  work on this stuff later
+for x in range(0, len(array_transaction_data)):  # For the number of transactions in the array
+    # Intermediary variables for total amounts sold in transaction
+    total_gallons = array_transaction_data[x].total_gallons  # total gallons for this transaction
+    total_22oz_bottles = 0
+    total_sixth_barrels = 0
+    # If the transaction is 22oz bottles
+    if array_transaction_data[x].individual_container_size == "22" and array_transaction_data[x].container_units == "oz":
+        total_22oz_bottles = array_transaction_data[x].number_units
 
+    # If the transaction is sixth barrels
+    if array_transaction_data[x].individual_container_size == "5.12" and array_transaction_data[x].container_units == "G":
+        total_sixth_barrels = array_transaction_data[x].number_units
 
+    # If the transaction is a retailer sale
+    if array_transaction_data[x].is_retailer_sale == 1:
+        # Increase product object data by amounts sold
+        product_id_dict[array_transaction_data[x].product_id].RTL_sixth_barrel_sold += total_sixth_barrels
+        product_id_dict[array_transaction_data[x].product_id].RTL_twentytwo_oz_bottle_sold += total_22oz_bottles
+        product_id_dict[array_transaction_data[x].product_id].RTL_total_gallons_sold += total_gallons
 
+    # If the transaction is not a retailer sale
+    if array_transaction_data[x].is_retailer_sale == 0:
+        if array_transaction_data[x].is_off_premise == "True":
+            product_id_dict[array_transaction_data[x].product_id].OFFP_total_gallons_sold += total_gallons
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# 1.   Inventory, Beginning of Month  (Line 6 on Prior Monthly Report)
-inv_beginning_of_month = prev_235_form.inv_end_of_month
-# 2.   Ale/Malt Liquor Brewed (Gallons Bottled & Kegged)
-beverage_brewed_monthly = 0.00
-# TODO calculate the amount in gallons added to inventory.xlsx during reporting period, output current_sheet b15
-beverage_brewed_ytd = 0.00
-# TODO sum beverage_brewed_monthly with C15 from previous ytd, output current_sheet c15
-
-
-# 3.   Ale/Malt Liquor Imported (Page 2, Schedule A)
-# always zero for us
-beverage_imported_monthly = 0.00
-beverage_imported_ytd = 0.00
-# TODO output current_sheet b16 and c16
-
-# 4.   Ale/Malt Liquor Returned from TX Wholesalers
-# always zero for us
-beverage_returned_from_wholesalers_monthly = 0.00
-# TODO output current_sheet b17
-
-# 5.   Total Ale/Malt Liquor Available (Sum of Lines 1,2,3,4)
-beverage_total_available_monthly = inv_beginning_of_month + \
-                                   beverage_brewed_monthly + \
-                                   beverage_imported_monthly + \
-                                   beverage_returned_from_wholesalers_monthly
-# TODO output to current_sheet b18
-
-# 6.   Inventory, End of Month
-inv_end_of_month = 0.00
-# TODO calculate end of month inventory from inventory.xlsx
-# TODO output to current_sheet b19
-
-# 7.   Wholesaler Sales (Page 2, line 2)
-# always zero for us
-wholesaler_sales_monthly = 0.00
-wholesaler_sales_ytd = 0.00
-# TODO output to current_sheet b20 and c20
-
-# 8.   Other Exemptions (Page 2, line 3)
-# we will calculate these manually as one-offs if there ever are any
-other_exemptions_monthly = 0.00
-other_exemptions_ytd = 0.00
-# TODO output to current_sheet b21 and c21
-
-# 9.   Total Exemptions (Sum of Lines 6, 7 & 8)
-beverage_total_exemptions_monthly = inv_end_of_month + wholesaler_sales_monthly + other_exemptions_monthly
-# TODO output to current_sheet b22
-
-# 10.  Ale/Malt Liquor Subject to Taxation (Line 5 minus Line 9)
-beverage_subject_to_taxation_monthly = beverage_total_available_monthly - beverage_total_exemptions_monthly
-beverage_subject_to_taxation_ytd = (prev_235_form.beverage_taxable_ytd + beverage_subject_to_taxation_monthly)
-
-# 11. Tax Rate Per Gallon
-tax_rate_per_gal = prev_235_form.tax_rate_per_gallon
-
-# Compliance Totals
-# 12.  Total Ale/Malt Liquor Sold to Retailers (Page 3)
-# TODO generate Brand Summary Sold sheet inputs from transactions.xlsx
-# calculate total values monthly and ytd
-# output to current_sheet B26 and C26
-
-# 13.  Total Ale/Malt Liquor Sold for On-Premise Consumption
-# TODO generate total ale/malt liquor sold on-prem from transactions.xlsx
-# calculate total values monthly and ytd
-# output to current_sheet B27 and C27
-
-# 14.  Total Ale/Malt Liquor Sold for Off-Premise Consumption
-# TODO generate total ale/malt liquor sold off-prem from transactions.xlsx
-# calculate total values monthly and ytd
-# output to current_sheet B28 and C28
-
-# 15.  Total Taxable Sales* (Sum of Lines 12,13, & 14)
-
-# TODO calculate total taxable sales monthly
-# output to current_sheet B29
-
-gross_tax_due = beverage_subject_to_taxation_monthly * tax_rate_per_gal
-# TODO output to current_sheet B32
-
-discount_tax_due = gross_tax_due - (gross_tax_due * .02)
-# TODO output to current_sheet B33
-
-less_authorized_credits = discount_tax_due
-# TODO output to current_sheet B34
-
-tax_due_state = discount_tax_due
-# TODO output to current_sheet B35
+        if array_transaction_data[x].is_off_premise == "False":
+            product_id_dict[array_transaction_data[x].product_id].ONP_total_gallons_sold += total_gallons
 
 
-# test_product_list(productsPath)
-'''
+# Write all data to current 235 form object
+
+# Write all data to current 236 form object
+
+# Output data to 235 form page 1
+
+# Output data to 235 form page 3
+
+# Output data to 236 form page 1
+
+# Output data to 236 form page 3
---
+++ formulas.py
@@ -0,0 +1,91 @@
+
+# 1.   Inventory, Beginning of Month  (Line 6 on Prior Monthly Report)
+inv_beginning_of_month = prev_235_form.inv_end_of_month
+# 2.   Ale/Malt Liquor Brewed (Gallons Bottled & Kegged)
+beverage_brewed_monthly = 0.00
+# TODO calculate the amount in gallons added to inventory.xlsx during reporting period, output current_sheet b15
+beverage_brewed_ytd = 0.00
+# TODO sum beverage_brewed_monthly with C15 from previous ytd, output current_sheet c15
+
+
+# 3.   Ale/Malt Liquor Imported (Page 2, Schedule A)
+# always zero for us
+beverage_imported_monthly = 0.00
+beverage_imported_ytd = 0.00
+# TODO output current_sheet b16 and c16
+
+# 4.   Ale/Malt Liquor Returned from TX Wholesalers
+# always zero for us
+beverage_returned_from_wholesalers_monthly = 0.00
+# TODO output current_sheet b17
+
+# 5.   Total Ale/Malt Liquor Available (Sum of Lines 1,2,3,4)
+beverage_total_available_monthly = inv_beginning_of_month + \
+                                   beverage_brewed_monthly + \
+                                   beverage_imported_monthly + \
+                                   beverage_returned_from_wholesalers_monthly
+# TODO output to current_sheet b18
+
+# 6.   Inventory, End of Month
+inv_end_of_month = 0.00
+# TODO calculate end of month inventory from inventory.xlsx
+# TODO output to current_sheet b19
+
+# 7.   Wholesaler Sales (Page 2, line 2)
+# always zero for us
+wholesaler_sales_monthly = 0.00
+wholesaler_sales_ytd = 0.00
+# TODO output to current_sheet b20 and c20
+
+# 8.   Other Exemptions (Page 2, line 3)
+# we will calculate these manually as one-offs if there ever are any
+other_exemptions_monthly = 0.00
+other_exemptions_ytd = 0.00
+# TODO output to current_sheet b21 and c21
+
+# 9.   Total Exemptions (Sum of Lines 6, 7 & 8)
+beverage_total_exemptions_monthly = inv_end_of_month + wholesaler_sales_monthly + other_exemptions_monthly
+# TODO output to current_sheet b22
+
+# 10.  Ale/Malt Liquor Subject to Taxation (Line 5 minus Line 9)
+beverage_subject_to_taxation_monthly = beverage_total_available_monthly - beverage_total_exemptions_monthly
+beverage_subject_to_taxation_ytd = (prev_235_form.beverage_taxable_ytd + beverage_subject_to_taxation_monthly)
+
+# 11. Tax Rate Per Gallon
+tax_rate_per_gal = prev_235_form.tax_rate_per_gallon
+
+# Compliance Totals
+# 12.  Total Ale/Malt Liquor Sold to Retailers (Page 3)
+# TODO generate Brand Summary Sold sheet inputs from transactions.xlsx
+# calculate total values monthly and ytd
+# output to current_sheet B26 and C26
+
+# 13.  Total Ale/Malt Liquor Sold for On-Premise Consumption
+# TODO generate total ale/malt liquor sold on-prem from transactions.xlsx
+# calculate total values monthly and ytd
+# output to current_sheet B27 and C27
+
+# 14.  Total Ale/Malt Liquor Sold for Off-Premise Consumption
+# TODO generate total ale/malt liquor sold off-prem from transactions.xlsx
+# calculate total values monthly and ytd
+# output to current_sheet B28 and C28
+
+# 15.  Total Taxable Sales* (Sum of Lines 12,13, & 14)
+
+# TODO calculate total taxable sales monthly
+# output to current_sheet B29
+
+gross_tax_due = beverage_subject_to_taxation_monthly * tax_rate_per_gal
+# TODO output to current_sheet B32
+
+discount_tax_due = gross_tax_due - (gross_tax_due * .02)
+# TODO output to current_sheet B33
+
+less_authorized_credits = discount_tax_due
+# TODO output to current_sheet B34
+
+tax_due_state = discount_tax_due
+# TODO output to current_sheet B35
+
+
+# test_product_list(productsPath)
